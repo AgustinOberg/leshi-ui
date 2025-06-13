fd7013358087d164fb1536456616f3ae3d6cf526
Agustin Oberg <154552167+codeagus11@users.noreply.github.com>
2025-06-13 11:06:46 -0300

    feat: demo

--- apps/demo/app/_layout.tsx
+++ apps/demo/app/_layout.tsx
@@ -1,5 +1,36 @@
+import { useInit } from "@/hooks/useInit";
+import { ThemeProvider, useTheme } from "@leshi/ui-rn";
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const App = () => {
+  const { colors } = useTheme();
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.card,
+        },
+        headerTintColor: colors.foreground,
+        contentStyle: {
+          backgroundColor: colors.background,
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home Screen" }} />
+    </Stack>
+  );
+};
+
+const RootLayout = () => {
+  const { isLoading } = useInit();
+  if (isLoading) {
+    return null;
+  }
+  return (
+    <ThemeProvider defaultMode="manual">
+      <App />
+    </ThemeProvider>
+  );
+};
+
+export default RootLayout;
